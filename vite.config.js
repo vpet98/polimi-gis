0fbc87c6fd2a922a45cdeee7c4fc7c78155dee84
Vasilis Petrakopoulos <vasilis4998@gmail.com>
2025-07-04 01:21:48 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite';
+import { globSync } from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-export default defineConfig({
+export default {
   base: '/polimi-gis/',
+
+  publicDir: 'public',
+
   build: {
-    sourcemap: true,
-  }
-});
+    target: 'ES2022',
+    outDir: 'docs',
+
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        ...Object.fromEntries(
+          globSync('pages/*.html').map(file => [
+            path.relative(
+              'pages',
+              file.slice(0, file.length - path.extname(file).length)
+            ),
+            fileURLToPath(new URL(file, import.meta.url))
+          ])
+        ),
+      },
+    },
+  },
+};
